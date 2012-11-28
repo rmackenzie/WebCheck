7b1e43ed33eb858e2020970f0e98c60694d46304
rmackenzie <ron.mackenzie@gmail.com>
2012-11-28 11:23:06 -0500

    Added delete account functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
 	private
 
 	def current_user
-  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		begin
+			@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		rescue ActiveRecord::RecordNotFound
+			return false
+		end	
 	end
 	
 end
